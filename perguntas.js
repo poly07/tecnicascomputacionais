90a6ffce9e6ce3de3b91b84e5720d1ca39e75ed9
poly07 <117644103+poly07@users.noreply.github.com>
2024-09-04 18:29:09 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "1. Qual lado da Força você prefere?",
+        alternativas: [
+            {
+                texto: "A) Lado da Luz",
+                afirmacao: [
+                    "O Guardião. Você valoriza a justiça, a paz e a harmonia. Prefere seguir um caminho de disciplina e altruísmo, sempre pensando no bem maior. Para você, ajudar os outros é mais importante do que obter poder."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Lado Sombrio",
+                afirmacao: [
+                    "O Ambicioso. Você é movido pela paixão e pelo desejo de alcançar seus objetivos a qualquer custo. Não tem medo de explorar o seu potencial ao máximo, mesmo que isso signifique quebrar algumas regras pelo caminho."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Qual é o seu lugar favorito para explorar?",
+        alternativas: [
+            {
+                texto: "Tatooine",
+                afirmacao: [
+                    "O Sobrevivente. Você é resiliente e se adapta bem a ambientes desafiadores. Prefere lugares que testem sua força e perseverança, e vê cada obstáculo como uma oportunidade para crescer.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Coruscant",
+                afirmacao: [
+                    "O Cosmopolita. Você é uma pessoa que adora estar no centro da ação e das decisões importantes. Prefere ambientes urbanos e vibrantes, onde sempre há algo novo acontecendo."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual o seu tipo de mentor ideal?",
+        alternativas: [
+            {
+                texto: "Yoda",
+                afirmacao: [
+                    "O Sábio. Você valoriza a sabedoria e o aprendizado ao longo da vida. Prefere conselhos baseados na experiência e na paciência, e acredita que o verdadeiro poder vem do conhecimento e do autocontrole."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Darth Vader",
+                afirmacao: [
+                    "O Poderoso. Você se inspira na força e na determinação. Aprecia mentores que lideram com autoridade e não hesitam em tomar decisões difíceis. Para você, a liderança é sobre mostrar força e comando."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você lida com conflitos?",
+        alternativas: [
+            {
+                texto: "Negociação",
+                afirmacao: [
+                    "O Pacificador. Você prefere resolver os conflitos de maneira diplomática, buscando o diálogo e a compreensão. Acredita que a cooperação é sempre uma opção e prefere evitar confrontos diretos."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Combate",
+                afirmacao: [
+                    "O Guerreiro. Você não foge de uma boa luta e acredita que às vezes a força é necessária para resolver os problemas. Para você, enfrentar os desafios de frente é a melhor maneira de conquistar respeito e autoridade."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu papel ideal em uma equipe?",
+        alternativas: [
+            {
+                texto: "Líder",
+                afirmacao: [
+                    "O Estrategista. Você gosta de estar no comando e se sente à vontade para tomar decisões difíceis. Prefere guiar os outros com seu senso de direção e responsabilidade, sempre planejando os próximos passos."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Piloto",
+                afirmacao: [
+                    "O Executor. Você é prático e gosta de estar na linha de frente. Prefere agir rapidamente e tomar decisões no momento, confiando na sua habilidade e reflexos para superar qualquer desafio."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu tipo de nave preferido?",
+        alternativas: [
+            {
+                texto: "X-Wing",
+                afirmacao: [
+                    "O Herói. Você é corajoso e gosta de aventuras, preferindo naves rápidas e versáteis que podem se adaptar a qualquer situação. Para você, é mais importante ser ágil e estar pronto para qualquer desafio."
+                ],
+            },
+            {
+                texto: "Star Destroyer",
+                afirmacao: [
+                    "O Dominador. Você prefere o poder e a presença imponente. Gosta de ter controle e de demonstrar força, preferindo naves grandes e poderosas que simbolizam autoridade e controle sobre qualquer situação."
+                ],
+            },
+        ]
+    },
+];
